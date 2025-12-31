0ebd38431b8fb460d6e25928d4314fc6eabccf49
EGBOH TOBECHUKWU <tobechukwuegboh@gmail.com>
2025-12-31 21:54:36 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+  
+  if (extname === '.css') {
+    contentType = 'text/css';
+  }
+  
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 - Page Not Found</h1><p>The page you are looking for does not exist.</p>');
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
